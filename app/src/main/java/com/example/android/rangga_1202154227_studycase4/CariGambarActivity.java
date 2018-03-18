5d4fd47be479810a4336e2f0ed5c4251667a2762
ranggaardi <rangga.ardi123@gmail.com>
2018-03-18 23:07:27 +0700

    ganti cari gambar

--- app/src/main/java/com/example/android/rangga_1202154227_studycase4/CariGambarActivity.java
+++ app/src/main/java/com/example/android/rangga_1202154227_studycase4/CariGambarActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.rangga_1202154227_studycase4;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +12,12 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.InputStream;
+import java.net.URL;
+
 public class CariGambarActivity extends AppCompatActivity {
     ImageView gambar;
-    EditText URL;
+    EditText cariLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +25,32 @@ public class CariGambarActivity extends AppCompatActivity {
         setContentView(R.layout.cari_gambar);
 
         gambar = (ImageView)findViewById(R.id.img);
-        URL = (EditText)findViewById(R.id.cari);
+        cariLink = (EditText)findViewById(R.id.cari);
 
     }
     public void CariGambar(View view){
-        Picasso.with(CariGambarActivity.this).load(URL.getText().toString()).placeholder(R.mipmap.ic_launcher_round)
-                .error(R.mipmap.ic_launcher).into(gambar);
+        new tampilGambar().execute();
+    }
+    class tampilGambar extends AsyncTask<Void, Void, Bitmap> {
+        protected Bitmap doInBackground(Void... voids) {
+
+            String link = cariLink.getText().toString();
+
+            Bitmap map = null;
+            try {
+
+                map = BitmapFactory.decodeStream((InputStream)new URL(link).getContent());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return map;
+        }
+        protected void onPostExecute(Bitmap bitmap) {
+            gambar.setImageBitmap(bitmap);
+        }
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
     }
 }
